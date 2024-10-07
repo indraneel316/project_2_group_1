7266050dfd88c0ce44cd9eca02684d017a34a501
indraneel316 <indraneel316@gmail.com>
2024-10-06 19:01:12 -0700

    port change

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/auth/users', userRoutes);
 app.use('/api/users', updateProfile);
 
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
