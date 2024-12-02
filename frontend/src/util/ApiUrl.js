6b1255096c4d5ac8b474a19b8a13d87992dde43b
indraneel316 <indraneel316@gmail.com>
2024-12-02 02:30:56 -0800

    initial commit

--- frontend/src/util/ApiUrl.js
+++ frontend/src/util/ApiUrl.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export const getApiUrl = async () => {
-    // const ec2Url = 'https://foodhealthnutrininja.com/backend';
+    const gaeUrl = 'https://nutrininja-443507.uw.r.appspot.com/backend';
     const localhostUrl = 'http://localhost:5000/backend';
 
     try {
-        await axios.get(`${localhostUrl}/healthcheck`);
-        return localhostUrl;
+        await axios.get(`${gaeUrl}/healthcheck`);
+        return gaeUrl;
     } catch (error) {
-        console.error('EC2 instance not available, falling back to localhost');
+        console.error('GAE instance not available, falling back to localhost');
         return localhostUrl;
     }
 };
